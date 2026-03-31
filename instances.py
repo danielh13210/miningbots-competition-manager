9ea0a20f877b7a568c9775dfa1b1bcc0aaa6ec78
daniel <51289681+danielh13210@users.noreply.github.com>
2026-04-01 07:26:02 +0800

    Use the new name

--- instances.py
+++ instances.py
@@ -4,7 +4,7 @@ traefik_rule_matcher=re.compile(r'traefik\..*\.rule')
 get_host=re.compile(r'Host\("(.*)"\)')
 def start_player(player,instance):
     with httpx.Client(transport=httpx.HTTPTransport(uds="/var/run/docker.sock")) as client:
-        response = client.post(f"http://localhost/containers/{player}-{instance}/start",timeout=httpx.Timeout(30.0))
+        response = client.post(f"http://localhost/containers/{instance}-{player}/start",timeout=httpx.Timeout(30.0))
 
         try:
             content=response.json()
@@ -13,7 +13,7 @@ def start_player(player,instance):
         return {'success':response.status_code==204 or response.status_code==304,'rawError':content} # return true if success
 def stop_player(player,instance):
     with httpx.Client(transport=httpx.HTTPTransport(uds="/var/run/docker.sock")) as client:
-        response = client.post(f"http://localhost/containers/{player}-{instance}/stop",timeout=httpx.Timeout(30.0))
+        response = client.post(f"http://localhost/containers/{instance}-{player}/stop",timeout=httpx.Timeout(30.0))
         try:
             content=response.json()
         except:
@@ -21,7 +21,7 @@ def stop_player(player,instance):
         return {'success':response.status_code==204 or response.status_code==304,'rawError':content} # return true if success
 def get_testserver_info(player,instance):
     with httpx.Client(transport=httpx.HTTPTransport(uds="/var/run/docker.sock")) as client:
-        response = client.get(f"http://localhost/containers/{player}-{instance}/json",timeout=httpx.Timeout(30.0))
+        response = client.get(f"http://localhost/containers/{instance}-{player}/json",timeout=httpx.Timeout(30.0))
         try:
             content=response.json()
         except:
